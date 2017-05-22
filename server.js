766651b81d759de242b3edc0d544fd383d0ebdf7
BastienBellariva <bastien.bellariva@ynov.com>
2017-05-22 16:09:44 +0200

    first parser for form

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 //Appel d'Express
-let app = require('express')()
+let express = require('express')
+let app = express()
 
 //Appel de EJS
 app.set('view engine', 'ejs')
+
+//Appel du CSS
+app.use('/assets', express.static('public'))
 
 //Page d'accueil
 app.get('/', (request, response) => {
     response.render('pages/index', {test: "Salut"})   
 })
 
-app.listen(8000)
+//Gestion du la task
+app.post('/', (request, response) => {
+    console.log(request)
+})
+
+app.listen(8000) 
